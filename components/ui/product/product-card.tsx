7fccfa6c66edfa1b292064af533abcd96f4216c7
Leonardo Citton <leocitton@gmail.com>
2023-12-16 23:49:33 +0100

    debug done - ui page 1/2

--- components/ui/product/product-card.tsx
+++ components/ui/product/product-card.tsx
@@ -23,7 +23,7 @@ export default function ProductCard({ item = null } : { item: any }) {
                         initial={{ opacity: 0, y: 8 }}
                         animate={{ opacity: 1, y: 0 }}
                         exit={{ opacity: 0, y: -8 }}
-                        transition={{ duration: 0.10, ease: 'easeOut' }}
+                        transition={{ duration: 0.05, ease: 'easeOut' }}
                     >
                         <TypingAnimaitonWords words={item?.description} />
                     </motion.div>
