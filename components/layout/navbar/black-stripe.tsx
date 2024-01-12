e93799298d8b0bd9c4863ba892892390990d5c79
Leonardo Citton <leocitton@gmail.com>
2024-01-12 21:07:36 +0100

    debug feature - dinamic announcements bar

--- components/layout/navbar/black-stripe.tsx
+++ components/layout/navbar/black-stripe.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import 'swiper/css';
 import 'swiper/css/grid';
 import 'swiper/css/pagination';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 export default function BlackStripe({ announcements }: { announcements: any }) {
@@ -15,6 +15,24 @@ export default function BlackStripe({ announcements }: { announcements: any }) {
   }
 
   const [array, setArray] = useState([])
+  const [deliveryAnnouncement, setDeliveryAnnouncement] = useState(null)
+  const [chatAnnouncement, setChatAnnouncement] = useState(null)
+
+  const findFirstDeliveryAndChatAnnouncements = (array) => {
+    array.map((announcement) => {
+      if (announcement.target === 'delivery') {
+        setDeliveryAnnouncement(announcement)
+        return
+      }
+    })
+
+    array.map((announcement) => {
+      if (announcement.target === 'link' && announcement.link === 'https://www.hevoluta.com/chat') {
+        setChatAnnouncement(announcement)
+        return
+      }
+    })
+  }
 
   useEffect(() => {
     const array = []
@@ -56,6 +74,7 @@ export default function BlackStripe({ announcements }: { announcements: any }) {
 
       // adding announcement to array
       array.push({
+        id: announcement.id,
         text,
         link,
         newsletter,
@@ -65,8 +84,8 @@ export default function BlackStripe({ announcements }: { announcements: any }) {
 
     // setting announcements to array
     setArray(array)
+    findFirstDeliveryAndChatAnnouncements(array)
   }, [])
-
 
   return (
     <>
@@ -88,7 +107,7 @@ export default function BlackStripe({ announcements }: { announcements: any }) {
             marginRight: 'auto'
           }}
         >
-          {array.map((announcement, index) => (
+          {array.map((announcement) => (
             <SwiperSlide>
               {(announcement.target === 'text' || announcement.target === 'delivery') && (
                 <button className="whitespace-nowrap font-mono text-12 font-medium">
@@ -117,22 +136,61 @@ export default function BlackStripe({ announcements }: { announcements: any }) {
       </div>
 
       {/* black stripe - desktop */}
-      <div className="hidden w-full max-w-full flex-nowrap items-center justify-between gap-10 overflow-x-auto bg-black px-5 py-1.5 text-white lg:flex">
-        <p className="whitespace-nowrap font-mono text-12 font-medium">
-          Spedizione gratuita da 65€
-        </p>
-        <button 
-          className="whitespace-nowrap font-mono text-12 font-medium hover:underline"
-          onClick={() => goToNewsletterForm()}
-        >
-          Iscriviti per avere il 10% di sconto primo ordine
+      <div className="hidden lg:flex items-center justify-center gap-2.5 bg-black px-5 py-1.5 text-white">
+        {/* slider go back button */}
+        <button className="swiper-button-prev-announcement-bar text-white">
+          <svg width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M13.25 8.75L9.75 12L13.25 15.25"></path>
+          </svg>
         </button>
-        <Link
-          href="/chat"
-          className="whitespace-nowrap font-mono text-12 font-medium hover:underline"
+
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={0}
+          modules={[Autoplay, Navigation]}
+          autoplay={{
+            delay: 5000,
+            disableOnInteraction: false
+          }}
+          navigation={{
+            nextEl: '.swiper-button-next-announcement-bar',
+            prevEl: '.swiper-button-prev-announcement-bar',
+          }}
+          className="mySwiper text-center"
         >
-          Dubbi? Chatta con noi gratis
-        </Link>
+          {array.map((announcement) => (
+            <SwiperSlide>
+              {(announcement.target === 'text' || announcement.target === 'delivery') && (
+                <button className="whitespace-nowrap font-mono text-12 font-medium">
+                  {announcement.text}
+                </button>
+              )}
+              {announcement.target === 'link' && (
+                <Link 
+                  href={announcement.link} 
+                  className="whitespace-nowrap font-mono text-12 font-medium hover:underline"
+                >
+                    {announcement.text}
+                </Link>
+              )}
+              {announcement.target === 'newsletter' && (
+                <button 
+                  className='whitespace-nowrap font-mono text-12 font-medium hover:underline'
+                  onClick={() => goToNewsletterForm()}
+                >
+                  {announcement.text}
+                </button>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* slider go next button */}
+        <button className="swiper-button-next-announcement-bar text-white">
+          <svg width="20" height="20" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M10.75 8.75L14.25 12L10.75 15.25"></path>
+          </svg>
+        </button>
       </div>
     </>
   );
